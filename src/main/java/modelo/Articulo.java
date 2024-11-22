5c3051799eaac1c7fc4ca355462ddf1748477de0
ninaaaa3 <kbbb0303@gmail.com>
2024-11-22 09:47:35 -0300

    actualización artículo

--- src/main/java/modelo/Articulo.java
+++ src/main/java/modelo/Articulo.java
@@ -9,5 +9,67 @@ package modelo;
  * @author kbarr
  */
 public class Articulo {
+    private int trackID;
+    private String trackName;
+    private String description;
+    private double unitPrice;
+    private int stock;
+    private String category;
+    private String currencyType;
+
+    public int getTrackID() {
+        return trackID;
+    }
     
+    public void setTrackID(int trackID) {
+        this.trackID = trackID;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(String currencyType) {
+        this.currencyType = currencyType;
+    }
 }
