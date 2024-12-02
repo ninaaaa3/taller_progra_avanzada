7af2a45d157d11ca658121fee7131691217c6c3f
Demis <demisrijks00@gmail.com>
2024-12-02 01:32:01 -0300

    .

---
+++ src/main/java/modelo/ExchangeRate.java
@@ -0,0 +1,60 @@
+package modelo;
+
+import java.sql.Timestamp;
+
+public class ExchangeRate {
+    private int id;
+    private String baseCurrency;
+    private String targetCurrency;
+    private double rate;
+    private Timestamp lastUpdated;
+    private int invoiceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(String targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public Timestamp getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Timestamp lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(int invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+}
